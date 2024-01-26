4d11658aced3efbeedb760630f0bec8fe209f81d
vitormarcolino <vitormarcolino2@gmail.com>
2024-01-26 16:39:35 -0300

    Exercicio_ts

---
+++ Exercicio_ts/main.js
@@ -0,0 +1,9 @@
+"use strict";
+function Multiplica1(numero1, numero2) {
+    return numero1 * numero2;
+}
+function DizOlá(nome) {
+    return `${nome}` + ` diz olá`;
+}
+console.log(Multiplica1(5, 8));
+console.log(DizOlá("Vitor"));
---
+++ Exercicio_ts/main.ts
@@ -0,0 +1,10 @@
+function Multiplica1 (numero1: number, numero2: number) {
+    return numero1 * numero2;
+}
+
+function DizOlá (nome: string) {
+    return `${nome}` + `diz olá`;
+}
+
+console.log(Multiplica1(5, 8));
+console.log(DizOlá("Vitor"));
